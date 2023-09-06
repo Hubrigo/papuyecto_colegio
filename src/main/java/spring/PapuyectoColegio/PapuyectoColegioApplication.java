d9e412983bca510f10bbc34d9bb52425d21d431f
hubrigo <hugofebrero1@hotmail.com>
2023-09-06 18:43:33 -0500

    first commit

---
+++ src/main/java/spring/PapuyectoColegio/PapuyectoColegioApplication.java
@@ -0,0 +1,13 @@
+package spring.PapuyectoColegio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapuyectoColegioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapuyectoColegioApplication.class, args);
+	}
+
+}
